e664e567b2bc1e0130806793a21b650bc3dea576
atis02 <guwancikramow@gmail.com>
2024-09-08 18:14:19 +0500

    Initial commit

---
+++ api/bot.js
@@ -0,0 +1,42 @@
+const TelegramBot = require("node-telegram-bot-api");
+require("dotenv").config();
+
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const bot = new TelegramBot(token);
+
+bot.setWebHook(`${process.env.BOT_URL}/api/bot`);
+
+module.exports = (req, res) => {
+  if (req.method === "POST") {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(404);
+  }
+};
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  const options = {
+    reply_markup: {
+      inline_keyboard: [[{ text: "Send Photo", callback_data: "send_photo" }]],
+    },
+  };
+
+  bot.sendMessage(chatId, "Click the button to receive a photo:", options);
+});
+
+bot.on("callback_query", (callbackQuery) => {
+  const message = callbackQuery.message;
+  const data = callbackQuery.data;
+
+  if (data === "send_photo") {
+    bot.sendPhoto(
+      message.chat.id,
+      "https://i.postimg.cc/fy9j3wZw/1704795878786-1704795856319.jpg",
+      {
+        caption: "Here is your photo!",
+      }
+    );
+  }
+});
